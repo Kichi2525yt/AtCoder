540972d9dc8b974b473b757265a8145036eab293
Kichi <Kichi_2525yt@outlook.jp>
2018-10-09 01:04:10 +0900

    [ABC112]2完

---
+++ ABC112/D_cpp/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <complex>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define var auto
+#define ll long long
+#define FOR(i, a, b) for(int (i) = (a); (i) < (b); (i)++)
+#define rep(i, n) for(int (i) = 0; (i) < (n); (i)++) 
+#define inp(a) int a; cin >> a;
+#define inps(s) std::string s; cin >> s;
+#define out(a) cout<<a<<endl;
+#define pb(a) push_back((a))
+#define endl "\n"
+
+ll conv(const string& num) { stringstream ss; ss << num << flush; ll n; ss >> n; return n; }
+string conv(const ll n) { stringstream ss; ss << n << flush; return ss.str(); }
+
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+
+
+	return 0;
+}
