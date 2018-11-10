d7d3c706a9a342ff49d0f051ad7a14b3d5693480
Kichi <Kichi_2525yt@outlook.jp>
2018-11-10 19:46:58 +0900

    edited header.cpp

--- header.cpp
+++ header.cpp
@@ -20,13 +20,14 @@ using namespace std;
 #define ll long long
 #define FOR(i, a, b) for(int (i) = (a); (i) < (b); (i)++)
 #define rep(i, n) for(int (i) = 0; (i) < (n); (i)++) 
-#define inp(a) int a; cin >> a;
-#define inps(s) std::string s; cin >> s;
+#define in(a) int a; cin >> a;
+#define instr(s) std::string s; cin >> s;
 #define out(a) cout<<a<<endl;
 #define pb(a) push_back((a))
 #define endl "\n"
 #define vi vector<int>
-
+#define invi(vec, n) vi(vec)(n); invec((vec), (n));
+#define invec(vec, n) rep(i, (n)) cin >> vec[i];
 
 int main() {
 	cin.tie(nullptr);
@@ -36,7 +37,6 @@ int main() {
 
 	return 0;
 }
-
 
 ll conv(const string num) { stringstream ss; ss << num << flush; ll n; ss >> n; return n; }
 string conv(const ll n) { stringstream ss; ss << n << flush; return ss.str(); }
