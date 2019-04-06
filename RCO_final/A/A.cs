20a332cc06772e7d609d46f5eb8f61a04b41c16e
Kichi <Kichi_2525yt@outlook.jp>
2019-04-07 00:33:48 +0900

    [RCO本選オープン]

---
+++ RCO_final/A/A.cs
@@ -0,0 +1,469 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using static System.Math;
+using static Marathon.Methods;
+using static Marathon.cin;
+// ReSharper disable LoopCanBeConvertedToQuery
+// ReSharper disable JoinDeclarationAndInitializer
+// ReSharper disable MemberCanBeMadeStatic.Local
+// ReSharper disable PossibleNullReferenceException
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+// ReSharper disable ArrangeTypeMemberModifiers
+// ReSharper disable SuggestVarOrType_BuiltInTypes
+// ReSharper disable SuggestVarOrType_Elsewhere
+// ReSharper disable InvertIf
+// ReSharper disable InconsistentNaming
+// ReSharper disable ConvertIfStatementToSwitchStatement
+// ReSharper disable UseObjectOrCollectionInitializer
+// ReSharper disable TailRecursiveCall
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable InlineOutVariableDeclaration
+// ReSharper disable FunctionRecursiveOnAllPaths
+#pragma warning disable
+
+namespace Marathon
+{
+    public class Program
+    {
+        private readonly Random rand = new Random();
+
+        private Stopwatch stopwatch;
+
+        private const int LIMIT = 1880;
+        private StreamWriter sw;
+
+        /* 入力値 */
+        private const int N = 50, T = 10000;
+
+        /* 出力値 */
+
+        /* フィールド */
+        private int[] field;
+        private readonly Dictionary<int, int> cards = new Dictionary<int, int>();
+        private int dist = T;
+        private int now;
+        private int last = -1;
+        private int point;
+
+
+        private void Input()
+        {
+            int _ = ReadInt;
+            _ = ReadInt;
+            field = Enumerable.Repeat(-1, N).ToArray();
+        }
+
+        private void Calc()
+        {
+            var flag = true;
+            while (flag)
+            {
+                var f = true;
+                for (int i = 0; i < N; i++)
+                {
+                    int cur = field[i];
+                    if (cur == -1)
+                    {
+                        cur = Check(i);
+                        now = i;
+                        if (dist <= 0)
+                        {
+                            flag = false;
+                            f = false;
+                            break;
+                        }
+
+                        dist -= Abs(i - now);
+                    }
+
+                    if (last == cur)
+                    {
+                        point += cur;
+                        field[i] = Check(i);
+                        last = field[i];
+                        f = false;
+                        break;
+                    }
+
+                    field[i] = cur;
+                    if (cards.ContainsKey(cur))
+                    {
+                        var next = cards[cur];
+                        Check(next);
+                        point += field[next];
+                        field[next] = -1;
+                        if (Abs(cur - next) > dist)
+                        {
+                            End();
+                            flag = false;
+                            f = false;
+                            break;
+                        }
+
+
+                        field[next] = -1;
+
+                        dist -= Abs(cur - next);
+                        now = next;
+                        cards.Remove(cur);
+                    }
+                    else
+                    {
+                        cards.Add(cur, i);
+                        last = cur;
+                    }
+                }
+
+                if (stopwatch.ElapsedMilliseconds > LIMIT)
+                {
+                    End();
+                    return;
+                }
+            }
+        }
+
+        private int Check(int i)
+        {
+            sw.WriteLine(i);
+#if !LOCAL
+            Console.Out.Flush();
+#endif
+            return ReadInt;
+        }
+
+        private void End()
+        {
+            sw.WriteLine(-1);
+#if !LOCAL
+            Console.Out.Flush();
+#endif
+        }
+
+        private void Solve()
+        {
+            stopwatch = Stopwatch.StartNew();
+            sw =
+#if LOCAL
+                new StreamWriter("out.txt");
+#else
+            new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+#endif
+            Console.SetOut(sw);
+            Input();
+            Calc();
+            Console.Read();
+        }
+
+        private readonly int[] dx = {-1, 0, 0, 1};
+        private readonly int[] dy = {0, 1, -1, 0};
+        private const int MOD = 1000000007;
+
+        /// <summary>aとbをスワップする</summary>
+        private static void Swap<T>(ref T a, ref T b) where T : struct
+        {
+            var tmp = b;
+            b = a;
+            a = tmp;
+        }
+
+        /// <summary>aとbの最大公約数を求める</summary>
+        private static long Gcd(long a, long b)
+        {
+            if (a < b) Swap(ref a, ref b);
+            return a % b == 0 ? b : Gcd(b, a % b);
+        }
+
+        /// <summary>aとbの最小公倍数を求める</summary>
+        private static long Lcm(long a, long b) => a / Gcd(a, b) * b;
+
+        public static void Print(params object[] args)
+        {
+            foreach (var s in args)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void PrintBool(bool val, string yes = "Yes", string no = "No")
+            => Console.WriteLine(val ? yes : no);
+
+        public static void PrintDebug(params object[] args)
+            => Console.Error.WriteLine(string.Join(" ", args));
+
+        public static void Main(string[] args)
+        {
+            new Program().Solve();
+            Console.Read();
+        }
+    }
+
+
+    public class Position : IComparable<Position>, IEquatable<Position>
+    {
+        public Position(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        public int Row;
+        public int Column;
+
+        public int CompareTo(Position other)
+        {
+            throw new Exception();
+        }
+
+        public bool Equals(Position other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return Row == other.Row && Column == other.Column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((Position)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Row * 397) ^ Column;
+            }
+        }
+    }
+
+    public class Pair<T1, T2>
+    {
+        public Pair(T1 first, T2 second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public T1 first;
+        public T2 second;
+    }
+
+    public static class Methods
+    {
+        public static Pair<T1, T2> make_pair<T1, T2>(T1 f, T2 l) => new Pair<T1, T2>(f, l);
+
+        public static Trans Maketrans(string a, string b) => new Trans(a, b);
+        public static Trans Maketrans(string a, string b, string c) => new Trans(a, b, c);
+
+        public static string Translate(this string str, Trans trans)
+        {
+            var s = str;
+            for (int i = 0; i < trans.Before.Length; i++)
+            {
+                if (trans.Delete.Contains(trans.Before[i])) continue;
+                s = s.Replace(trans.Before[i], trans.After[i]);
+            }
+
+            return s;
+        }
+        
+        /// <summary>
+        /// 指定した値以上の先頭のインデクスを返す
+        /// </summary>
+        /// <typeparam name="T">比較する値の型</typeparam>
+        /// <param name="arr">対象の配列（※ソート済みであること）</param>
+        /// <param name="start">開始インデクス [inclusive]</param>
+        /// <param name="end">終了インデクス [exclusive]</param>
+        /// <param name="value">検索する値</param>
+        /// <param name="comparer">比較関数(インターフェイス)</param>
+        /// <returns>指定した値以上の先頭のインデクス</returns>
+        public static int LowerBound<T>(T[] arr, int start, int end, T value, IComparer<T> comparer)
+        {
+            int low = start;
+            int high = end;
+            while (low < high)
+            {
+                var mid = ((high - low) >> 1) + low;
+                if (comparer.Compare(arr[mid], value) < 0)
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+            return low;
+        }
+
+        /// <summary>
+        /// 指定した値以上の先頭のインデクスを返す
+        /// </summary>
+        /// <typeparam name="T">比較する値の型</typeparam>
+        /// <param name="arr">対象の配列（※ソート済みであること）</param>
+        /// <param name="value">検索する値</param>
+        /// <returns>指定した値以上の先頭のインデクス</returns>
+        public static int LowerBound<T>(T[] arr, T value) where T : IComparable
+        {
+            return LowerBound(arr, 0, arr.Length, value, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        /// 指定した値より大きい先頭のインデクスを返す
+        /// </summary>
+        /// <typeparam name="T">比較する値の型</typeparam>
+        /// <param name="arr">対象の配列（※ソート済みであること）</param>
+        /// <param name="start">開始インデクス [inclusive]</param>
+        /// <param name="end">終了インデクス [exclusive]</param>
+        /// <param name="value">検索する値</param>
+        /// <param name="comparer">比較関数(インターフェイス)</param>
+        /// <returns>指定した値より大きい先頭のインデクス</returns>
+        public static int UpperBound<T>(T[] arr, int start, int end, T value, IComparer<T> comparer)
+        {
+            int low = start;
+            int high = end;
+            while (low < high)
+            {
+                var mid = ((high - low) >> 1) + low;
+                if (comparer.Compare(arr[mid], value) <= 0)
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+            return low;
+        }
+
+        /// <summary>
+        /// 指定した値より大きい先頭のインデクスを返す
+        /// </summary>
+        /// <typeparam name="T">比較する値の型</typeparam>
+        /// <param name="arr">対象の配列（※ソート済みであること）</param>
+        /// <param name="value">検索する値</param>
+        /// <returns>指定した値より大きい先頭のインデクス</returns>
+        public static int UpperBound<T>(T[] arr, T value)
+        {
+            return UpperBound(arr, 0, arr.Length, value, Comparer<T>.Default);
+        }
+
+        public static bool IsEven(this int x) => x % 2 == 0;
+        public static bool IsOdd(this int x) => x % 2 != 0;
+        public static bool IsEven(this long x) => x % 2 == 0;
+        public static bool IsOdd(this long x) => x % 2 != 0;
+        public static double Log2(double x) => Log(x, 2);
+
+        public static T Min<T>(params T[] col) => col.Min();
+        public static T Max<T>(params T[] col) => col.Max();
+
+        public class Trans
+        {
+
+            public Trans(string a, string b)
+            {
+                if (a.Length != b.Length)
+                    throw new ArgumentException($"引数 {nameof(a)}と{nameof(b)}は同じ長さである必要があります。");
+                
+                Before = a.ToCharArray();
+                After = b.ToCharArray();
+                Delete = new char[0];
+            }
+
+            public Trans(string a, string b, string c) : this(a, b)
+            {
+                Delete = c.ToCharArray();
+            }
+
+            public char[] Before { get; }
+            public char[] After { get; }
+            public char[] Delete { get; }
+        }
+    }
+
+    public static class cin
+    {
+        private const char _separator = ' ';
+        private static readonly Queue<string> _input = new Queue<string>();
+        private static readonly StreamReader sr =
+#if FILE
+            new StreamReader("in.txt");
+#else
+            new StreamReader(Console.OpenStandardInput());
+#endif
+
+        public static string ReadLine => sr.ReadLine();
+
+        public static string ReadStr => Read;
+
+        public static string Read
+        {
+            get
+            {
+                if (_input.Count != 0) return _input.Dequeue();
+
+                // ReSharper disable once PossibleNullReferenceException
+                var tmp = sr.ReadLine().Split(_separator);
+                foreach (var val in tmp)
+                {
+                    _input.Enqueue(val);
+                }
+
+                return _input.Dequeue();
+            }
+        }
+
+        public static int ReadInt => int.Parse(Read);
+
+        public static long ReadLong => long.Parse(Read);
+
+        public static double ReadDouble => double.Parse(Read);
+
+        public static string[] StrArray() => ReadLine.Split(' ');
+
+        public static int[] IntArray() => ReadLine.Split(' ').Select(int.Parse).ToArray();
+
+        public static long[] LongArray() => ReadLine.Split(' ').Select(long.Parse).ToArray();
+
+        public static string[] StrArray(long n)
+        {
+            var ret = new string[n];
+            for (long i = 0; i < n; ++i) ret[i] = Read;
+            return ret;
+        }
+
+        public static int[] IntArray(long n) => StrArray(n).Select(int.Parse).ToArray();
+
+        public static long[] LongArray(long n) => StrArray(n).Select(long.Parse).ToArray();
+
+        static bool TypeEquals<T, U>() => typeof(T) == typeof(U);
+        static T ChangeType<T, U>(U a) => (T) System.Convert.ChangeType(a, typeof(T));
+
+        static T Convert<T>(string s) => TypeEquals<T, int>() ? ChangeType<T, int>(int.Parse(s))
+            : TypeEquals<T, long>() ? ChangeType<T, long>(long.Parse(s))
+            : TypeEquals<T, double>() ? ChangeType<T, double>(double.Parse(s))
+            : TypeEquals<T, char>() ? ChangeType<T, char>(s[0])
+            : ChangeType<T, string>(s);
+
+        public static void Multi<T>(out T a) => a = Convert<T>(ReadStr);
+
+        public static void Multi<T, U>(out T a, out U b)
+        {
+            var ar = StrArray(2);
+            a = Convert<T>(ar[0]);
+            b = Convert<U>(ar[1]);
+        }
+
+        public static void Multi<T, U, V>(out T a, out U b, out V c)
+        {
+            var ar = StrArray(3);
+            a = Convert<T>(ar[0]);
+            b = Convert<U>(ar[1]);
+            c = Convert<V>(ar[2]);
+        }
+    }
+}
